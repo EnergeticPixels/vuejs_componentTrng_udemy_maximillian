98738699b45c918b2d4d324b4cf286f23dbc41a0
Anthony Jackman <energeticpixels@gmail.com>
2017-01-22 12:46:27 -0600

    dymanic content in slots

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
       <div class='row'>
         <div class='col-xs-12'>
 			<app-quote>
-				<h2>The Quote</h2>
+				<h2>{{ quoteTitle }}</h2>
 				<p>A damn wonderful quote</p>
 			</app-quote>
         </div>
@@ -14,9 +14,14 @@
 <script>
     import Quote from './components/Quote.vue';
     export default {
-      components: {
-        appQuote: Quote
-      }
+		data: function() {
+			return {
+				quoteTitle: 'The Great Quote'
+			}
+		},
+		components: {
+			appQuote: Quote
+		}
     }
 </script>
 

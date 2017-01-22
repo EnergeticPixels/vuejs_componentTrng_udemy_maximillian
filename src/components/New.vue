3339c9ef28c0d8f1fd90e47cb463e7aea02b41a9
Anthony Jackman <energeticpixels@gmail.com>
2017-01-22 13:41:27 -0600

    testing for persistence

--- src/components/New.vue
+++ src/components/New.vue
@@ -1,12 +1,19 @@
 <template>
     <div>
         <h3>The new component</h3>
+        <button @click='counter++'>Increase</button>
+        <p>{{ counter }}</p>
     </div>
 </template>
 
 <script>
     console.log('from inside quote');
 	export default {
+        data: function() {
+            return {
+                counter: 0
+            };
+        }
 	}
 </script>
 

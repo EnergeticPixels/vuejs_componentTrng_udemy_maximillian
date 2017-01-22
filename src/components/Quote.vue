2c1ff520bc96d230efe51a3a778ac47288cf1cec
Anthony Jackman <energeticpixels@gmail.com>
2017-01-22 12:18:14 -0600

    basic component

---
+++ src/components/Quote.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>A famous quote</p>
+    </div>
+</template>
+
+<script>
+    console.log('from inside quote')
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid #ccc;
+        box-shadow: 1px 1px 2px black;
+        padding: 30px;
+        margin: 30px auto;
+		text-align: center;
+    }
+</style>
